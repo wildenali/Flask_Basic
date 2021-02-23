c1854bfebcac4279967c386a5e38f7e5e100d850
Wilden Ali <wildenwildenaliali@gmail.com>
2021-02-23 14:20:31 +0700

    Http method, POST method

---
+++ 02_HTTPMethods/A_post_method.py
@@ -0,0 +1,18 @@
+from flask import Flask, redirect, url_for, request
+app = Flask(__name__)
+
+@app.route('/succes/<name>')
+def success(name):
+    return 'welcome %s' % name
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['namanya']
+        return redirect(url_for('success', name = user))
+    else:
+        user = request.args.get('namanya')
+        return redirect(url_for('success', name = user))
+
+if __name__ == "__main__":
+    app.run(debug=True)
