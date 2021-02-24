55ea30cdf40a1b3e12052eaaea227b44e57758ff
Wilden Ali <wildenwildenaliali@gmail.com>
2021-02-24 10:17:58 +0700

    session for login and logout

---
+++ 07_Session/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, make_response, session, url_for, redirect
+app = Flask(__name__)
+app.secret_key = 'randomstringapapun'
+
+@app.route('/')
+def index():
+    search = request.args.get('search')
+    return render_template('index.html', search=search)
+
+@app.route('/profile/<username>')
+def show_profile(username):
+    return render_template('profile.html', username=username)
+
+@app.route('/login', methods=['GET', 'POST'])
+def show_login():
+    if request.method == 'POST':
+        res = make_response('Email kamu adalah ' + request.form['email'])
+        res.set_cookie('email_user', request.form['email'])
+        session['username'] = request.form['email']
+        return res
+    
+    if 'username' in session:
+        username = session['username']
+        return redirect(url_for('show_profile', username=username))
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('show_login'))
+
+@app.route('/getcookie')
+def get_cookie():
+    email = request.cookies.get('email_user')
+    return 'Email yang tersimpan di cookie adalah ' + email
+
+if __name__ == "__main__":
+    app.run(debug=True)
