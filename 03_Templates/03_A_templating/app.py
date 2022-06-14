f0b3382aaf5412cb8a76f89f2c1b29c93676d9a0
Wulan Panekenan <wulandaripanekenan@gmail.com>
2022-06-14 10:48:52 +0700

    html in flask

--- 03_Templates/03_A_templating/app.py
+++ 03_Templates/03_A_templating/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/index")
 def indexku():
-    return "<h1>Hallo Flask templating</h1>"
+    return "<h1>Hallo Flask templating</h1><br /> <h3>haha</h3>"
 
 if __name__ == "__main__":
     app.run(debug=True)
