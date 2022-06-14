bf7e7ea9d6ed83530f9d847edf6f2d0e1be83579
Wulan Panekenan <wulandaripanekenan@gmail.com>
2022-06-14 11:06:33 +0700

    pass the param in templates

--- 03_Templates/03_B_templates_nilai/app.py
+++ 03_Templates/03_B_templates_nilai/app.py
@@ -1,18 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template # render_template untuk pisahin html file dari sini, atau ambil template html di folder templates
 app = Flask(__name__)
 
-@app.route('/hello/<user>')
-def helo_name(user):
-    return render_template('hello.html', namanya = user)
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
-@app.route('/nilai/<int:nilai>')
+@app.route('/hello_name/<user>')
+def helo_name(user):
+    return render_template('helo_name.html', namanya = user)
+
+@app.route('/helo_nilaii/<int:nilai>')
 def helo_nilai(nilai):
-    return render_template('hello.html', berapanilainya = nilai)
+    return render_template('hello_nilai.html', berapanilainya = nilai)
 
 @app.route('/result')
 def result():
     dict = {'phy':50,'che':60,'maths':70}
-    return render_template('hello.html', hasil = dict)
+    return render_template('result.html', hasil = dict)
 
 
 if __name__ == '__main__':
